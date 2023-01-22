8df17a27936e0176bdca9c22dc9cbf19d2c0a2f8
unknown <yousef-1122@hotmail.com>
2023-01-22 22:21:54 +0300

    modify

--- src/main/java/org/example/RecipeSearch.java
+++ src/main/java/org/example/RecipeSearch.java
@@ -1,5 +1,6 @@
 package org.example;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -14,20 +15,10 @@ public class RecipeSearch {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter ingredients to search: ");
         String ingredients = scan.nextLine();
+
         try {
-            URL url = new URL("https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredients + "&apiKey=" + "b1270e9e454342f182aee416420012a0");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String Line;
-            StringBuilder content = new StringBuilder();
-            while ((Line = in.readLine()) != null) {
-                content.append(Line);
-            }
-            in.close();
-            con.disconnect();
-            
-            JSONArray results = new JSONArray(content.toString());
+            JSONArray results = getRecipe(ingredients);
+
             for (int i = 0; i < results.length(); i++) {
                 JSONObject recipe = results.getJSONObject(i);
                 System.out.println("Title: " + recipe.getString("title"));
@@ -35,5 +26,24 @@ public class RecipeSearch {
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    public static JSONArray getRecipe(String ingredients) throws IOException {
+
+        URL url = new URL("https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredients
+                + "&apiKey=" + "b1270e9e454342f182aee416420012a0");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String Line;
+        StringBuilder content = new StringBuilder();
+        while ((Line = in.readLine()) != null) {
+            content.append(Line);
+        }
+        in.close();
+        con.disconnect();
+
+        JSONArray results = new JSONArray(content.toString());
+        return results;
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,16 @@
 package org.example;
+import org.json.JSONArray;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+class RecipeSearchTest {
+    private final RecipeSearch Rs = new RecipeSearch();
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    @Test
+    void shouldReturnEggResults() throws IOException {
+        JSONArray results = RecipeSearch.getRecipe("egg");
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        assertEquals(results.toString() , Rs.getRecipe("egg").toString());
     }
 }
