d869a7e0e23e59cd2fc55847e92057888e0a4ff2
AbdulazizAlghamdi24 <aalghamdi2952@stu.kau.edu.sa>
2023-02-06 09:45:13 +0300

    Instruction

--- src/main/java/org/example/RecipeSearch.java
+++ src/main/java/org/example/RecipeSearch.java
@@ -21,7 +21,8 @@ public class RecipeSearch {
 
             for (int i = 0; i < results.length(); i++) {
                 JSONObject recipe = results.getJSONObject(i);
-                System.out.println("Title: " + recipe.getString("title"));
+                System.out.println(i+1+"- Title: " + recipe.getString("title"));
+                System.out.println("Instructions: " + getInstructions(recipe.getInt("id")));
             }
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
@@ -46,4 +47,31 @@ public class RecipeSearch {
         JSONArray results = new JSONArray(content.toString());
         return results;
     }
+    public static String getInstructions(int recipeId) throws IOException {
+        URL url = new URL("https://api.spoonacular.com/recipes/" + recipeId + "/analyzedInstructions?apiKey=" + "b1270e9e454342f182aee416420012a0");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String Line;
+        StringBuilder content = new StringBuilder();
+        while ((Line = in.readLine()) != null) {
+            content.append(Line);
+        }
+        in.close();
+        con.disconnect();
+
+        JSONArray instructionsArray = new JSONArray(content.toString());
+        StringBuilder instructions = new StringBuilder();
+        for (int i = 0; i < instructionsArray.length(); i++) {
+            JSONObject instruction = instructionsArray.getJSONObject(i);
+            JSONArray steps = instruction.getJSONArray("steps");
+            for (int j = 0; j < steps.length(); j++) {
+                JSONObject step = steps.getJSONObject(j);
+                instructions.append((j + 1) + ". " + step.getString("step") + "\n");
+            }
+        }
+
+        return instructions.toString();
+    }
+
 }
