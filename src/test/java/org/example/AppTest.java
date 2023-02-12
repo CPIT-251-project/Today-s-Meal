9d5a2910ada297746b5181d2443e2c54b86df614
unknown <yousef-1122@hotmail.com>
2023-02-12 22:01:06 +0300

    added tests

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,7 +4,12 @@ import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RecipeSearchTest {
@@ -14,13 +19,22 @@ class RecipeSearchTest {
     void shouldReturnEggResults() throws IOException {
         JSONArray results = RecipeSearch.getRecipe("egg");
         JSONAssert.assertEquals(results, Rs.getRecipe("egg"), true);
-        //assertEquals(results.toString() , Rs.getRecipe("egg").toString());
     }
     @Test
     void shouldReturnInstructions() throws IOException {
-        JSONArray results = RecipeSearch.getRecipe("egg");
-        JSONObject recipe = results.getJSONObject(1);
         assertEquals(RecipeSearch.getInstructions(1),Rs.getInstructions(1));
-        //JSONAssert.assertEquals(results, Rs.getRecipe("egg"), true);
+    }
+    @Test
+    void shouldReturnCal() throws IOException {
+        assertEquals(RecipeSearch.getCalories(1),Rs.getCalories(1));
+    }
+    @Test
+    void shouldReturnException() throws IOException, Exception {
+        String data = "a";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Scanner scan = new Scanner(System.in);
+
+        assertThrows(Exception.class, () ->{RecipeSearch.display(scan);});
+        assertThrows(Exception.class, () ->{RecipeSearch.search(scan, "egg");});
     }
 }
